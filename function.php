5cea60441a16b3059aec46b164767995aff6f565
Pier.W <factory666@live.it>
2019-04-18 00:11:21 +0200

    fix for old version of php

--- function.php
+++ function.php
@@ -5,8 +5,8 @@ include 'qa.php';
 function getQA($arr){
 
   foreach ($arr as $key => $value) {
-    echo "<h1>" . $value[Q] . "</h1>";
-    echo "<p>" . $value[A] . "</p>";
+    echo "<h1>" . $value["Q"] . "</h1>";
+    echo "<p>" . $value["A"] . "</p>";
   }
 }
  ?>
